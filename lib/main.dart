00c4de57b0457dd37f4fe0cd11b9dcc91c68bf7d
tech explorers <73522283+techexplorers123@users.noreply.github.com>
2025-08-31 19:02:36 +0530

    fixed the bugs and udpated the wear_plus lib as the other one was outdated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wear/wear.dart';
+import 'package:wear_plus/wear_plus.dart';
 import 'package:barcode_widget/barcode_widget.dart';
 
 void main() {
