50cf3adee7367fa2a84fe388f8bd60a8ce14c859
tech explorers <73522283+techexplorers123@users.noreply.github.com>
2025-09-01 17:06:19 +0530

    second prototype: added rotory dile support and multiple qr for testing along with some UI improvements.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:wear_plus/wear_plus.dart';
 import 'package:barcode_widget/barcode_widget.dart';
+import 'package:wearable_rotary/wearable_rotary.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +12,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,12 +25,44 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: MainPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final List<String> codes = ["2349", "5678", "9012", "3456"];
+  int selectedIndex = 0;
+  late final StreamSubscription<RotaryEvent> _rotarySubscription;
+  @override
+  void initState() {
+    super.initState();
+    _rotarySubscription = rotaryEvents.listen(handleRotaryEvent);
+  }
+
+  void handleRotaryEvent(RotaryEvent event) {
+    setState(() {
+      if (event.direction == RotaryDirection.clockwise) {
+        selectedIndex = (selectedIndex + 1) % codes.length;
+      } else {
+        selectedIndex = (selectedIndex - 1 + codes.length) % codes.length;
+      }
+      HapticFeedback.selectionClick();
+    });
+  }
+
+  @override
+  void dispose() {
+    _rotarySubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,16 +73,34 @@ class MainPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "qr1",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8),
-                BarcodeWidget(
-                  data: "2349",
-                  barcode: Barcode.code39(),
-                  width: 200,
-                  height: 80,
+                AnimatedSwitcher(
+                  duration: Duration(milliseconds: 400),
+                  transitionBuilder: (child, animation) => ScaleTransition(
+                    scale: animation,
+                    child: FadeTransition(opacity: animation, child: child),
+                  ),
+                  child: Column(
+                    key: ValueKey<int>(selectedIndex),
+                    children: [
+                      Text(
+                        "qr$selectedIndex",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      BarcodeWidget(
+                        data: codes[selectedIndex],
+                        barcode: Barcode.code39(),
+                        width: 180,
+                        height: 60,
+                        backgroundColor: Colors.black,
+                        margin: EdgeInsets.all(8),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
