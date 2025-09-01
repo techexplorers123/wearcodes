bdcd3e800336dd5011de6caeb6d83111c7c1970b
tech explorers <73522283+techexplorers123@users.noreply.github.com>
2025-09-01 17:17:17 +0530

    more UI / UX improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:wear_plus/wear_plus.dart';
 import 'package:barcode_widget/barcode_widget.dart';
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
           onSurface: Colors.white10,
         ),
       ),
-      home: MainPage(),
+      home: const MainPage(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -40,27 +39,37 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   final List<String> codes = ["2349", "5678", "9012", "3456"];
   int selectedIndex = 0;
+  late final PageController _pageController;
   late final StreamSubscription<RotaryEvent> _rotarySubscription;
+
   @override
   void initState() {
     super.initState();
+    _pageController = PageController(
+      initialPage: 1000 * codes.length,
+    ); // infinite loop trick
     _rotarySubscription = rotaryEvents.listen(handleRotaryEvent);
   }
 
   void handleRotaryEvent(RotaryEvent event) {
-    setState(() {
-      if (event.direction == RotaryDirection.clockwise) {
-        selectedIndex = (selectedIndex + 1) % codes.length;
-      } else {
-        selectedIndex = (selectedIndex - 1 + codes.length) % codes.length;
-      }
-      HapticFeedback.selectionClick();
-    });
+    if (event.direction == RotaryDirection.clockwise) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      _pageController.previousPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+    HapticFeedback.selectionClick();
   }
 
   @override
   void dispose() {
     _rotarySubscription.cancel();
+    _pageController.dispose();
     super.dispose();
   }
 
@@ -69,41 +78,40 @@ class _MainPageState extends State<MainPage> {
     return WatchShape(
       builder: (context, shape, child) {
         return Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedSwitcher(
-                  duration: Duration(milliseconds: 400),
-                  transitionBuilder: (child, animation) => ScaleTransition(
-                    scale: animation,
-                    child: FadeTransition(opacity: animation, child: child),
-                  ),
-                  child: Column(
-                    key: ValueKey<int>(selectedIndex),
-                    children: [
-                      Text(
-                        "qr$selectedIndex",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+          body: PageView.builder(
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                selectedIndex = index % codes.length;
+              });
+            },
+            itemBuilder: (context, index) {
+              final code = codes[index % codes.length];
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "qr$selectedIndex",
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      SizedBox(height: 8),
-                      BarcodeWidget(
-                        data: codes[selectedIndex],
-                        barcode: Barcode.code39(),
-                        width: 180,
-                        height: 60,
-                        backgroundColor: Colors.black,
-                        margin: EdgeInsets.all(8),
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 8),
+                    BarcodeWidget(
+                      data: code,
+                      barcode: Barcode.code39(),
+                      width: 180,
+                      height: 60,
+                      backgroundColor: Colors.black,
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.white,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           ),
         );
       },
